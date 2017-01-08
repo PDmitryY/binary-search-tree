5f57594ecf7a7e3f50079136885b38957ea5e051
Dmitry Petrochenkov <demon-st2002@yandex.ru>
2017-01-08 15:07:55 +0300

    initial

---
+++ test/BST_test.js
@@ -0,0 +1,13 @@
+var chai = require("chai");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var expect = chai.expect;
+chai.use(sinonChai);
+
+describe('Binary Search Tree', function() {
+	describe('insert', function() {
+		it('should do smth', function(){
+			expect(1).to.equal(1); 
+		});
+	});
+});
