ab96a8cc0a1351569e7b9c871d952242d19f2871
Dmitry Petrochenkov <demon-st2002@yandex.ru>
2017-01-18 18:58:04 +0300

    insert - final, search - final, contains - in work

--- BST.js
+++ BST.js
@@ -1,12 +1,9 @@
 'use strict'
 
+var Node = require("./Node");
+
 class BinarySearchTree {
-	constructor(value, key) {
-		this.value = value;
-		this.parent = null;
-		this.left = null;
-		this.right = null;
-		this.key = key;
+	constructor() {
 		this.root = null;
 	}
 
@@ -17,159 +14,136 @@ class BinarySearchTree {
 			return this.root;
 		}
 		else {
-			throw new Error('the tree does not exist');
+			throw new Error ('the tree does not exist');
 		}
-	};
+	}
 
 	//stores specified value in tree using key; method should be chainable
 
-	insert(value, key){
-
-		if(!this.root) {
-			this.root = key;
-			return this.root;
+	insert(key, value){
+		if (typeof key !== "number" || value === undefined) {
+			throw new Error('Entered data is not valid');
 		};
 
-		this.parent = this.root;
+		if(!this.root) {
+			this.root = new Node(key, value);
+			return this;
+		};
 
-		if (key < this.parent) {
-			this.left = key;
-			this.left.parent = this;
+		let leaf = this.root;
 
-			return this.left;
-		}
-		else {
-			this.right = key;
-			this.right.parent = this;
-			return this.right;
-		}		
+		while(true){
+			if(key < leaf.key){
+				if(!leaf.left){
+					leaf.left = new Node(key, value);
+					break;
+				};
+				leaf = leaf.left;
+				
+			} else if (key >= leaf.key){
+				if(!leaf.right){
+					leaf.right = new Node(key, value);
+					break;
+				};
+				leaf = leaf.right;
+			};
+		};
+		return this;
 
-	};
+	}
 
 	//removes node from tree by provided key; method should be chainable
 
 	delete(key){
-		let node = this.root;
-		let searchNode = search(key);
-		if (key == node ){
-			node = null;
-		}
-		else {
-			
-		}
-
-	};
+		
+	}
 
 	//looking for stored data in tree using key
 
 	search(key){
-		let node = this.root
-		while (node != null){
-			if (key < node){
-				node = node.left;
-			}
-			else if (key > node){
-				node = node.right;
-			}
-			else {
-				return node;
-			}
+		if (typeof key !== "number") {
+			throw new Error('Entered data is not valid')
 		};
-		return null;
-	};
+
+		if (!this.root) {
+			throw new Error('Tree does not exist')
+		};
+
+		if(this.root.key===key&&!this.root.left&&!this.root.right) {
+			return this.root;
+		};
+
+		let leaf = this.root;
+		let result = {};
+
+		while(true){
+			if(key < leaf.key){
+				if(leaf.left.key===key){
+					result = leaf.left;
+					break;
+				};
+				leaf = leaf.left;
+				
+			} else if (key >= leaf.key){
+				if(leaf.right.key===key){
+					result = leaf.right;
+					break;
+				};
+				leaf = leaf.right;
+			};
+		};
+		return result;
+	}
 
 	//returns whether BST contains such value or not
 
 	contains(value){
+		if (value === undefined) {
+			throw new Error('Entered data is not valid');
+		};
 
-	};
+		if (!this.root) {
+			throw new Error('Tree does not exist')
+		};
+
+		if(this.root.value===value) {
+			return true;
+		};
+
+
+		let leaf = this.root;
+
+		/*while(true){
+			if(key < leaf.key){
+				if(leaf.left.key===key){
+					result = leaf.left;
+					break;
+				};
+				leaf = leaf.left;
+				
+			} else if (key >= leaf.key){
+				if(leaf.right.key===key){
+					result = leaf.right;
+					break;
+				};
+				leaf = leaf.right;
+			};
+		};
+		return result;*/
+	}
 
 	//retrieves ordered sequence of stored values in given order (order is boolean)
 
 	traverse(order){
 
-	};
+	}
 
 	//verifies whether tree is well-formed binary search tree or not
 
 	verify(){
 
-	};
-
-/*
-	appendChild(node) {
-		if(!this.left) {
-			this.left = node;
-			this.left.parent = this;
-		}
-		else if(!this.right) {
-			this.right = node;
-			this.right.parent = this;
-		}
 	}
 
-	removeChild(node) {
-    if(node == this.left) {
-			this.left = null;
-		}
-    else if(node == this.right) {
-	    this.right = null;
-    }
-		else {
-			throw new Error('you are trying to remove node which does not exist');
-		}
-		node.parent = null;
-	}
-
-	remove() {
-		if(!this.parent) return;
-		this.parent.removeChild(this);
-	}
-
-	swapWithParent() {
-    if(!this.parent) return;
-
-		let parentOfParent = null;
-    	let parentBranch = null;
-
-		if(this.parent.parent) {
-			parentOfParent = this.parent.parent;
-
-			parentBranch = this.parent === parentOfParent.left ? 'left' : 'right';
-		};
-
-		const oldParent = this.parent;
-		oldParent.parent = this;
-
-		if(this.getSibling()){
-		  this.getSibling().parent = this;
-		}
-
-    	this.right = this.parent.right;
-		this.parent.left = this.left;
-		this.left = this.parent;
-
-		if(!parentOfParent) return;
-		  this.parent = parentOfParent;
-
-			this.parent[parentBranch] = this;
-	}
-
-	getSibling() {
-		if(!this.parent) return;
-		if(this === this.parent.left) {
-			return this.parent.right;
-		}
-		if(this === this.parent.right){
-			return this.parent.left;
-		}
-	}*/
 }
 
-let root = new BinarySearchTree();
-let left = root.insert(1, new BinarySearchTree());
-let right = root.insert(1, new BinarySearchTree());
-let leftLeft = left.insert(1, new BinarySearchTree());
-let leftRight = left.insert(1, new BinarySearchTree());
-
-console.log(root);
+module.exports = BinarySearchTree;
--- test/BST_test.js
+++ test/BST_test.js
@@ -1,13 +1,132 @@
 var chai = require("chai");
 var sinon = require("sinon");
 var sinonChai = require("sinon-chai");
+var BinarySearchTree = require("../BST");
+var Node = require("../Node");
 var expect = chai.expect;
 chai.use(sinonChai);
 
 describe('Binary Search Tree', function() {
+	describe('constructor', function(){
+		it('should initialize root', function(){
+			let bst = new BinarySearchTree();
+			expect(bst.root).to.equal(null);
+		});
+	});
 	describe('insert', function() {
-		it('should do smth', function(){
-			expect(1).to.equal(1); 
+		it('should throw an arror if key or value is undefined', function(){
+			let bst = new BinarySearchTree();
+			let shouldFall = false;
+
+			try{
+				bst.insert(undefined,"root_value");
+				shouldFall = true; 
+			} catch(e) {
+				
+			}
+			
+			expect(shouldFall).to.be.false;
+		});
+		it('should search for root', function(){
+			let bst = new BinarySearchTree();
+			expect(bst.root).to.equal(null);
+			bst.insert(5,"root_value");
+			expect(bst.root).to.be.not.null;
+			expect(bst.root).to.be.instanceof(Node);
+			expect(bst.root.key).to.equal(5);
+			expect(bst.root.value).to.equal("root_value");
+		});
+		it('should insert node in left branch of tree if node.key < root.key', function(){
+			let bst = new BinarySearchTree();
+			bst.insert(5, "root_value").insert(3, "left_value");
+
+			expect(bst.root.left).to.exist;
+			expect(bst.root.left.key).to.equal(3);
+		});
+		it('should insert node in right branch of tree if node.key >= root.key', function(){
+			let bst = new BinarySearchTree();
+			bst.insert(5, "root_value").insert(6, "right_value");
+			expect(bst.root.right).to.exist;
+			expect(bst.root.right.key).to.equal(6);
+		});
+		it('should insert node in right of the left branch if node.key < root.key && >= root.left.key', function(){
+			let bst = new BinarySearchTree();
+			bst.insert(5, "root_value").insert(2, "left_value").insert(3, "left_right_value");
+
+			expect(bst.root.left.key).to.equal(2);
+			expect(bst.root.left.right.key).to.equal(3);
+		});
+		it('should insert node in left of the right branch if node.key >= root.key && < root.left.key', function(){
+			let bst = new BinarySearchTree();
+			bst.insert(5, "root_value").insert(8, "left_value").insert(6, "left_right_value");
+
+			expect(bst.root.right.key).to.equal(8);
+			expect(bst.root.right.left.key).to.equal(6);
+		});
+	});
+	describe('delete', function(){
+		it('should ', function(){
+
+		});
+	});
+	describe('search', function(){
+		it('should throw an arror if key is undefined', function(){
+			let bst = new BinarySearchTree();
+			let shouldFall = false;
+
+			try{
+				bst.insert(undefined,"root_value");
+				shouldFall = true; 
+			} catch(e) {
+				
+			}
+			
+			expect(shouldFall).to.be.false;
+		});
+		it('should check if type of result of the search is object', function(){
+			let bst = new BinarySearchTree();
+			bst.insert(10, "root_value");
+			let key = 10;
+			expect(bst.search(key)).to.be.instanceof(Object);
+		});
+		it('should return root if root.key == key', function(){
+			let bst = new BinarySearchTree();
+			bst.insert(10, "root_value");
+			expect(bst.search(10).key).to.equal(10);
+		});
+		it('should return root.left if root.left.rey == key', function(){
+			let bst = new BinarySearchTree();
+			bst.insert(10, "root_value").insert(8, "left_value").insert(15, "right_value").insert(9, "left_right_value");
+			expect(bst.search(8).key).to.equal(8);
+		});
+		it('should return root.right.left if root.right.left.key == key', function(){
+			let bst = new BinarySearchTree();
+			bst.insert(10, "root_value").insert(8, "left_value").insert(15, "right_value").insert(9, "left_right_value").insert(12, "right_left.left_right_value");
+			expect(bst.search(12).key).to.equal(12);
+		});
+	});
+	describe('contains', function(){
+		it('should throw error if value is undefined', function(){
+			let bst = new BinarySearchTree();
+			let shouldFall = false;
+
+			try{
+				bst.insert(key, undefined);
+				shouldFall = true;
+			}
+			catch(e){
+			};
+			expect(shouldFall).to.be.false;
+		});
+		it('should throw an error if tree does not exist', function(){
+			let bst = new BinarySearchTree();
+			let left_right_value = 'root_value';
+			expect(bst.contains()).to.throw(new Error ('Tree does not exist'));
+		});
+		it('should return true if root contains value', function(){
+			let bst = new BinarySearchTree();
+			bst.insert(10, "root_value").insert(8, "left_value").insert(15, "right_value").insert(9, "left_right_value");
+			expect(bst.contains("root_value")).to.be.true;
 		});
 	});
 });
